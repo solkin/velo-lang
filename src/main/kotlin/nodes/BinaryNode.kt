0b642474da301aad3af9cfee6e101371b6fe52a4
Igor Solkin <inbox@tomclaw.com>
2023-03-08 16:29:32 +0300

    Add string concatenation

--- src/main/kotlin/nodes/BinaryNode.kt
+++ src/main/kotlin/nodes/BinaryNode.kt
@@ -14,21 +14,8 @@ data class BinaryNode(
     )
 
     private fun applyOp(op: String, a: Any, b: Any): Any {
-        fun num(n: Any): Double {
-            return (n as? Double) ?: throw IllegalArgumentException("Expected number but got $n")
-        }
-        fun bool(n: Any): Boolean {
-            return (n as? Boolean) ?: throw IllegalArgumentException("Expected boolean but got $n")
-        }
-        fun div(n: Any): Double {
-            val v = num(n)
-            if (v == 0.0) {
-                throw IllegalArgumentException("Division by zero")
-            }
-            return v
-        }
         return when (op) {
-            "+" -> (num(a) + num(b))
+            "+" -> if (a.isNum() && b.isNum()) (num(a) + num(b)) else (a.toString() + b.toString())
             "-" -> (num(a) - num(b))
             "*" -> (num(a) * num(b))
             "/" -> (num(a) / div(b))
@@ -44,4 +31,22 @@ data class BinaryNode(
             else -> throw IllegalArgumentException("Can't apply operator $op")
         }
     }
+}
+
+private fun Any.isNum() = this is Double
+
+private fun num(n: Any): Double {
+    return (n as? Double) ?: throw IllegalArgumentException("Expected number but got $n")
+}
+
+private fun bool(n: Any): Boolean {
+    return (n as? Boolean) ?: throw IllegalArgumentException("Expected boolean but got $n")
+}
+
+private fun div(n: Any): Double {
+    val v = num(n)
+    if (v == 0.0) {
+        throw IllegalArgumentException("Division by zero")
+    }
+    return v
 }
