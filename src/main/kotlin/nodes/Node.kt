46e267f5ce0eb87d42feffb324be1b6d17ea48cd
Igor Solkin <inbox@tomclaw.com>
2023-08-20 20:24:09 +0300

    Add string property for any type

--- src/main/kotlin/nodes/Node.kt
+++ src/main/kotlin/nodes/Node.kt
@@ -14,6 +14,7 @@ abstract class Type<T>(private val t: T) {
 
     open fun property(name: String, args: List<Type<*>>?): Type<*> {
         return when (name) {
+            "string" -> StrType(value().toString())
             "ext" -> {
                 if (args?.size != 1) {
                     throw IllegalArgumentException("Property 'ext' requires one named func argument")
