027474bb71240613528b37772117af9b9958ce75
Igor Solkin <inbox@tomclaw.com>
2025-06-09 00:09:04 +0300

    Add stream input

---
+++ src/main/kotlin/compiler/parser/StreamInput.kt
@@ -0,0 +1,51 @@
+package compiler.parser
+
+import java.io.InputStream
+
+class StreamInput(val input: InputStream) : Input {
+
+    private var line = 0
+    private var col = 0
+    private var next: Char? = null
+
+    override fun peek(): Char {
+        return next ?: nextChar()
+    }
+
+    override fun next(): Char {
+        val ch = next ?: nextChar()
+        when (ch) {
+            Character.MIN_VALUE -> {}
+            '\n', '\r' -> {
+                line++
+                col = 0
+            }
+
+            else -> {
+                col++
+            }
+        }
+        next = null
+        return ch
+    }
+
+    override fun eof(): Boolean {
+        return peek() == Character.MIN_VALUE
+    }
+
+    override fun croak(msg: String) {
+        throw Error("$msg ($line:$col)")
+    }
+
+    private fun nextChar(): Char {
+        val r = input.read()
+        val c = if (r == -1) {
+            Character.MIN_VALUE
+        } else {
+            r.toChar()
+        }
+        next = c
+        return c
+    }
+
+}
---
+++ src/test/kotlin/StreamInputTest.kt
@@ -0,0 +1,54 @@
+import compiler.parser.Input
+import compiler.parser.StreamInput
+import java.lang.Character.MIN_VALUE
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class StreamInputTest {
+
+    @Test
+    fun testPeek() {
+        val input: Input = StreamInput("12".byteInputStream())
+        val ch1 = input.peek()
+        val ch2 = input.peek()
+        assertEquals(ch1, '1')
+        assertEquals(ch2, '1')
+    }
+
+    @Test
+    fun testRead() {
+        val input: Input = StreamInput("12".byteInputStream())
+        val ch1 = input.next()
+        val ch2 = input.next()
+        val ch3 = input.next()
+        assertEquals(ch1, '1')
+        assertEquals(ch2, '2')
+        assertEquals(ch3, MIN_VALUE)
+    }
+
+    @Test
+    fun testReadPeek() {
+        val input: Input = StreamInput("12".byteInputStream())
+        val ch1 = input.next()
+        val ch2 = input.peek()
+        val ch3 = input.peek()
+        val ch4 = input.next()
+        assertEquals('1', ch1)
+        assertEquals('2', ch2)
+        assertEquals('2', ch3)
+        assertEquals('2', ch4)
+    }
+
+    @Test
+    fun testEof() {
+        val input: Input = StreamInput("12".byteInputStream())
+        val ch1 = input.next()
+        val eof1 = input.eof()
+        val ch2 = input.next()
+        val eof2 = input.eof()
+        assertEquals(ch1, '1')
+        assertEquals(ch2, '2')
+        assertEquals(eof1, false)
+        assertEquals(eof2, true)
+    }
+}
