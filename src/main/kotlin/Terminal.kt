8ef3ce2ef5fe8214a2d9a48e6971d4b78c799883
Igor Solkin <inbox@tomclaw.com>
2025-06-18 20:38:44 +0300

    Fix terminal class print method result

--- src/main/kotlin/Terminal.kt
+++ src/main/kotlin/Terminal.kt
@@ -1,9 +1,8 @@
 import java.util.Scanner
 
 class Terminal() {
-    fun print(text: String): String {
+    fun print(text: String) {
         kotlin.io.print(text)
-        return text
     }
     fun input(): String {
         return Scanner(System.`in`).nextLine()
