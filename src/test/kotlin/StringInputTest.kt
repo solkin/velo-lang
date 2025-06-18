effb6a1cfa502b1fbb83b376468f2fe7b4c9f179
Igor Solkin <inbox@tomclaw.com>
2025-06-19 01:38:17 +0300

    Add string input mark & reset tests

--- src/test/kotlin/StringInputTest.kt
+++ src/test/kotlin/StringInputTest.kt
@@ -40,6 +40,36 @@ class StringInputTest {
     }
 
     @Test
+    fun testMarkReset() {
+        val input: Input = StringInput("123")
+        val ch1 = input.next()
+        input.mark()
+        val ch2 = input.next()
+        val ch3 = input.next()
+        input.reset()
+        val ch4 = input.next()
+        assertEquals('1', ch1)
+        assertEquals('2', ch2)
+        assertEquals('3', ch3)
+        assertEquals('2', ch4)
+    }
+
+    @Test
+    fun testMarkResetPeek() {
+        val input: Input = StringInput("123")
+        val ch1 = input.next()
+        input.mark()
+        val ch2 = input.next()
+        val ch3 = input.next()
+        input.reset()
+        val ch4 = input.peek()
+        assertEquals('1', ch1)
+        assertEquals('2', ch2)
+        assertEquals('3', ch3)
+        assertEquals('2', ch4)
+    }
+
+    @Test
     fun testEof() {
         val input: Input = StringInput("12")
         val ch1 = input.next()
